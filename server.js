5f90f821ad05459bba2e501e74ad8b8f93c03552
hicks123 <korir.isaiah@gmail.com>
2019-01-30 16:40:39 +0300

    fixed heroku depl error heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mysite'));
+app.use(express.static(__dirname + '/dist/weatherMis'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/mysite/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/weatherMis/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
